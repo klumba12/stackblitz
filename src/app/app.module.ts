5e8cb75935c7ca6da6157139c7f984e709531b09
klumba12 <dimitry.sitnov@gmail.com>
2021-03-03 20:45:06 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSelectModule } from "@angular/material/select";
+
+import { AppComponent } from "./app.component";
+import { GridModule, PipeModule } from "ng2-qgrid";
+import { ThemeModule } from "ng2-qgrid/theme/material";
+
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatIconModule,
+
+    GridModule,
+    ThemeModule,
+    PipeModule
+  ]
+})
+export class AppModule {}
